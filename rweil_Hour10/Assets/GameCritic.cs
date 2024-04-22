6ee8035026f0aefa34c1d647ad0f3249f1ff58d5
SinfulPk4 <ryder.pk4@gmail.com>
2024-04-21 22:58:09 -0700

    Chaos Ball

---
+++ rweil_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,21 @@
+(/*                                                                                                              
+  ,ad8888ba,                                                ,ad8888ba,             88         88             
+ d8"'    `"8b                                              d8"'    `"8b            ""   ,d    ""             
+d8'                                                       d8'                           88                   
+88            ,adPPYYba, 88,dPYba,,adPYba,   ,adPPYba,    88            8b,dPPYba, 88 MM88MMM 88  ,adPPYba,  
+88      88888 ""     `Y8 88P'   "88"    "8a a8P_____88    88            88P'   "Y8 88   88    88 a8"     ""  
+Y8,        88 ,adPPPPP88 88      88      88 8PP"""""""    Y8,           88         88   88    88 8b          
+ Y8a.    .a88 88,    ,88 88      88      88 "8b,   ,aa     Y8a.    .a8P 88         88   88,   88 "8a,   ,aa  
+  `"Y88888P"  `"8bbdP"Y8 88      88      88  `"Ybbd8"'      `"Y8888Y"'  88         88   "Y888 88  `"Ybbd8"'   
+    
+  Improving the game!
+    The game is fairly easy, knocking the balls into the goals isn't much of a challenge.
+    Adding some static obsticles could make the game harder.
+    Adding movement mechanics similar to rocket league to the bumper could make the game more interesting.
+  The fun part of the game is seeing the balls slingshot around sometimes, not too tedious unless the ball gets stuck behind the bumper.
+
+Changed the movement sound to the old minecraft splash noise! :)
+ Improved design of bumper, changing both shape and out of many shapes.
+Added floor surrounding arena and bleachers .
+Created super chaos ball, glowing super fast ball that bounces off all other surfaces.    
+  */
