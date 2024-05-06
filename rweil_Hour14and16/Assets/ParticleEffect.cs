916bb18bb53a99ecf9dbc54829cdbbfab94a779f
SinfulPk4 <ryder.pk4@gmail.com>
2024-05-06 01:21:58 -0700

    Particles

---
+++ rweil_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,5 @@
+/* Changed shape to sphere
+ Emission Rate over Time to 2000 because it looks like fireworks
+Color over lifetime starting at green to while to pink
+Added collision with a plane on the floor to show said collision
+Simulation speed of 3*/
