15caf2deb30e0b5201c3bfeda9a7090ece5dfe37
SinfulPk4 <ryder.pk4@gmail.com>
2024-05-19 23:49:28 -0700

    Runner

---
+++ rweil_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,10 @@
+/*
+ Game Critic!!
+The game is fairly easy. Difficulty wise 1/5
+Changes to make it harder could be changing the severity of the obsticles or adding obsticles to duck under
+Adding more movement options and sound effects can add a wow factor 
+Two fun aspects are trying to get as far as possible and dodging the obsticles
+The most tedious parts were the size of the obsticles and the chance of them spawning right next to a power up
+
+I changed both pickups, the power up is an atom like sphere, and the obsticle is a ufo.
+ */
